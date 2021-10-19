6cda4d236b7f2b9b8884123d0de542423a43a46c
Eduard <dumpweed10@gmail.com>
2021-10-19 16:10:47 +0300

    catalog

--- js/tabs.js
+++ js/tabs.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('.stages-link').forEach(function(tabsBtn) {
     tabsBtn.addEventListener('click', function(event) {
       const path = event.currentTarget.dataset.path
-      console.log(path)
 
       document.querySelectorAll('.tab-content').forEach(function(tabContent) {
         tabContent.classList.remove('tab-content-active')
