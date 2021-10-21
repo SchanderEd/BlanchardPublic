dffa0bdaabb72d40893f2705311f19c0f47713e6
Eduard <dumpweed10@gmail.com>
2021-10-21 13:39:47 +0300

    btn

--- js/catalog-btn.js
+++ js/catalog-btn.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    document.querySelectorAll(".catlog__tab-btn").forEach(item => {
+    document.querySelectorAll(".catalog__tab-btn").forEach(item => {
         item.addEventListener("click", function() {
-            item.classList.toggle("catlog__tab-btn--active")
+            item.classList.toggle("catalog__tab-btn--active")
             item.blur();
         })
         
