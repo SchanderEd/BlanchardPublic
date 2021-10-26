a88bdd37a6c65a26fac4ddfb56caa82cf293782f
Eduard <dumpweed10@gmail.com>
2021-10-26 19:19:38 +0300

    catalogartist

--- js/catalog-btn.js
+++ js/catalog-btn.js
@@ -1,11 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelectorAll(".catalog__tab-btn").forEach(item => {
-        item.addEventListener("click", function() {
-            item.classList.toggle("catalog__tab-btn--active")
+    const list = document.querySelectorAll('.catalog__tab-btn')
+    list.forEach(item => {
+        item.addEventListener('click', (e) => {
+            list.forEach(el => { el.classList.remove('catalog__tab-btn--active'); });
+            item.classList.add('catalog__tab-btn--active')
             item.blur();
+
         })
-        
     })
-    
+
+    const artist = document.querySelectorAll('.catalog__accordion-btn-artist')
+    artist.forEach(artistItem => {
+        artistItem.addEventListener('click', (e) => {
+            artist.forEach(el => { el.classList.remove('catalog__accordeon-btn--active'); });
+            artistItem.classList.add('catalog__accordeon-btn--active')
+            artistItem.blur();
+        })
+    })
+
 })
